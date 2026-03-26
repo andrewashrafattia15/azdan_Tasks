420639bbc4d0efe76f0efc5a17b6adaefdc9a915
Andrew Ashraf <andrewashrafattia15@gmail.com>
2026-03-26 11:47:08 +0200

    NCIC - Invoice printout "Production"

---
+++ NCIC/Production/Invoice/24-3-2026/az_ncic_ue_create_print_fields.js
@@ -0,0 +1,154 @@
+/**
+ *@NApiVersion 2.1
+ *@NScriptType UserEventScript
+ */
+define(["./az_ats_amount_in_words", 'N/ui/serverWidget', 'N/record','N/search', 'N/file', "N/query"], function (Amount_converter, serverWidget, record, search,file, query) {
+
+    const beforeLoad = (context) => {
+        try {
+
+            if (context.type === context.UserEventType.PRINT) {
+                const Record = context.newRecord;
+                const myRec = Record.id;
+                const tranDate = Record.getValue('trandate');
+                const tenancyContractId = Record.getValue("custbody_ino_pms_tenancy_contract");
+                if (tranDate) {
+                    const hijriDate = convertToHijri(tranDate);
+                    setSearchValueInspection(context, 'custpage_hijri_date', 'Hijri Date', hijriDate)
+
+                }
+
+                if (tenancyContractId) {
+                    const lcNumber = getNumberByTC(tenancyContractId,myRec);
+                    if (lcNumber) {
+                        setSearchValueInspection(context, 'custpage_lc_number', 'LC Number', lcNumber)
+
+                    }
+                }
+
+                const lchargeID = Record.getSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'custcol_ino_pms_lease_charge',
+                    line: 0
+                });
+                const leaseChargeNum= getLeaseCharge(lchargeID);
+                setleaseCharge(context,leaseChargeNum);
+            }
+        } catch (errBeforeLoad) {
+            log.debug("errBeforeLoad", errBeforeLoad);
+        }
+    }
+
+    const beforeSubmit = (context) => {
+        try {
+            const rec = context.newRecord;
+            const invoiceTotal = rec.getValue('total');
+
+            if (invoiceTotal) {
+                let amount_in_words_arabic = Amount_converter.amount_to_arabic_words(invoiceTotal);
+                rec.setValue({
+                    fieldId: 'custbody_ino_re_amount_in_words_ar',
+                    value: amount_in_words_arabic
+                });
+            }
+        } catch (errBeforeSubmit) {
+            log.debug("errBeforeSubmit", errBeforeSubmit);
+        }
+    };
+
+   const getLeaseCharge = (lchargeID) => {
+        try {
+            if (!lchargeID) return '';
+
+            const lookup = search.lookupFields({
+                type: 'customrecord_ino_pms_tc_invs_schd',
+                id: lchargeID,
+                columns: ['custrecord_ino_pms_lc_number']
+            });
+
+            let lcNumber = '';
+
+            if (lookup.custrecord_ino_pms_lc_number) {
+                lcNumber = lookup.custrecord_ino_pms_lc_number;
+            }
+
+            return lcNumber;
+
+        } catch (errorGetLeaseCharge) {
+            log.debug("errorGetLeaseCharge", errorGetLeaseCharge);
+           
+        }
+    };
+
+    const convertToHijri = (date) => {
+        try {
+            const gDate = new Date(date);
+            const hijri = new Intl.DateTimeFormat('ar-SA-u-ca-islamic-umalqura', {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric'
+            }).format(gDate);
+
+            return hijri;
+
+        } catch (errorconvertToHijri) {
+            log.debug('errorconvertToHijri', errorconvertToHijri);
+
+        }
+
+    }
+
+    const getNumberByTC = (tenancyContractId,invId) => {
+        try {
+            const suiteQL = `
+                SELECT custrecord_ino_pms_lc_number
+                FROM customrecord_ino_pms_tc_invs_schd
+                WHERE custrecord_ino_pms_lc_type IN (17, 13, 5)
+                AND custrecord_ino_pms_lc_tc = ?
+                AND custrecord_ino_pms_lc_invoice_number = ?
+            `;
+
+            const resultSet = query.runSuiteQL({
+                query: suiteQL,
+                params: [tenancyContractId, invId]
+            }).asMappedResults();
+
+            if (resultSet && resultSet.length > 0) {
+                return resultSet[0].custrecord_ino_pms_lc_number || "";
+            }
+            return "";
+        } catch (errorgetNumberByTC) {
+            log.debug("errorgetNumberByTC", errorgetNumberByTC);
+            return "";
+        }
+    };
+
+    const setSearchValueInspection = (context, fieldId, label, value) => {
+
+        const hijriDateFld = context.form.addField({
+            id: fieldId,
+            type: serverWidget.FieldType.TEXT,
+            label: label
+        });
+
+        hijriDateFld.defaultValue = value;
+
+    }
+
+    const setleaseCharge = (context, leaseChargeNum) => {
+
+        const leaseCharge = context.form.addField({
+            id: 'custpage_lease_charge',
+            type: serverWidget.FieldType.TEXT,
+            label: 'Lease Charge'
+        });
+
+        leaseCharge.defaultValue = leaseChargeNum;
+    }
+
+    return {
+        beforeLoad: beforeLoad,
+        beforeSubmit: beforeSubmit
+    }
+
+})
