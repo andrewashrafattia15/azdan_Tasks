d2bd5c965118f54dafc9c0fdb585ffbea6809323
Andrew Ashraf <andrewashrafattia15@gmail.com>
2026-06-07 15:52:07 +0300

    CBC Production (Estimate)

---
+++ CBC/Production/Estimate/31-5-2026/az_cbc_cs_estimate_prt_btn.js
@@ -0,0 +1,18 @@
+/**
+ *@NApiVersion 2.1
+ *@NScriptType ClientScript
+ */
+ define(['N/log'], function (log) {
+
+
+    const onButtonClick = (id) => {
+        try {
+            window.open("/app/site/hosting/scriptlet.nl?script=2583&deploy=1&recId=" + id)
+        } catch (errorOnButtonClick) {
+            log.debug("errorOnButtonClick", errorOnButtonClick);
+        }
+    }
+    return {  
+        pageInit: () => {return true;},
+        onButtonClick };
+});
